34acde2092860cbcc43314005dc25dd363e93c47
Dojassola <gabriel_costa17@estudante.sesisenai.org.br>
2025-06-11 21:39:03 -0300

    commit

--- routes/relatorio.js
+++ routes/relatorio.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as relatorio_controller from '../controllers/relatorio_controller.js';
 import { verifyToken, authorizeRoles } from '../middlewares/auth.js';
-import { verificarVagas } from '../middlewares/verificarvagas.js';
+import { verificarVagas } from '../middlewares/validate.js';
 const router = Router();
 
 router.use(verifyToken);
